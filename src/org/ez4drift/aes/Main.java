a37db5c1a2f52e079a9cf87fabb45d9d41d8ea84
EZ4DRIFT <792005029@qq.com>
2021-09-06 16:20:23 +0800

    简单单线程AES加解密算法命令行版

---
+++ src/org/ez4drift/aes/Main.java
@@ -0,0 +1,120 @@
+package org.ez4drift.aes;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String option;
+        String key;
+        String IV;
+        String inputFilePath;
+        String outputFilePath;
+
+        // 开始界面
+        divider();
+        System.out.println("AES Encrypt and Decrypt Program by EZ4DRIFT");
+        divider();
+        System.out.println("#       option");
+        System.out.println("1       encrypt");
+        System.out.println("2       decrypt");
+        System.out.println("other   quit");
+        divider();
+        System.out.print("Please choose your option:");
+
+        option = scanner.nextLine();
+
+        if (option.contentEquals("1")) {
+            divider();
+
+            System.out.println("Input your password. Anything is OK, but You should remember it:");
+            key = scanner.nextLine();
+            key = MD5Util.stringToMD5(key);
+
+            divider();
+
+            System.out.println("Input your bias vector, the requirements are the same as above:");
+            IV = scanner.nextLine();
+            IV = MD5Util.stringToMD5(IV);
+
+            divider();
+
+            System.out.println("Input the full path of the input file that you want to encrypt:");
+            inputFilePath = scanner.nextLine();
+
+            divider();
+
+            System.out.println("Input the full path of the output file. If it's empty, it will use a default value:");
+            outputFilePath = scanner.nextLine();
+
+            divider();
+
+            if (outputFilePath.contentEquals("")) {
+                try {
+                    AESCore aesCore = new AESCore(key, IV, 1);
+                    aesCore.encryptFile(inputFilePath);
+                    System.out.println("OK, the file has been encrypted with the suffix '.aes' in the same directory.");
+                } catch (Exception e) {
+                    System.out.println("Error! Maybe the path of the input file was wrong.");
+                }
+            } else {
+                try {
+                    AESCore aesCore = new AESCore(key, IV, 1);
+                    aesCore.encryptFile(inputFilePath, outputFilePath);
+                    System.out.println("OK, the file has been encrypted.");
+                } catch (Exception e) {
+                    System.out.println("Error! Maybe the path of the input file or the output file was wrong.");
+                }
+            }
+        } else if (option.contentEquals("2")) {
+            divider();
+
+            System.out.println("Input your password:");
+            key = scanner.nextLine();
+            key = MD5Util.stringToMD5(key);
+
+            divider();
+
+            System.out.println("Input your bias vector:");
+            IV = scanner.nextLine();
+            IV = MD5Util.stringToMD5(IV);
+
+            divider();
+
+            System.out.println("Input the full path of the input file that you want to decrypt:");
+            inputFilePath = scanner.nextLine();
+
+            divider();
+
+            System.out.println("Input the full path of the output file:");
+            outputFilePath = scanner.nextLine();
+
+            divider();
+
+            try {
+                AESCore aesCore = new AESCore(key, IV, 1);
+                aesCore.decryptFile(inputFilePath, outputFilePath);
+                System.out.println("OK, the file has been decrypted.");
+                System.out.println("But if the password or the vector was wrong, it would be a mess.");
+            } catch (Exception e) {
+                System.out.println("Error! Maybe the path of the input file or the output file was wrong.");
+            }
+        }
+
+        divider();
+
+        System.out.println("Press \"Enter\" to quit.");
+
+        divider();
+
+        scanner.nextLine();
+
+        scanner.close();
+    }
+
+    private static void divider() {
+        System.out.println("===================================================================================");
+    }
+}
